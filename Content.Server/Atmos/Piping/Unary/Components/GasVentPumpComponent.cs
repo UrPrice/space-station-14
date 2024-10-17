6a683a3fec2914a28161d85e1cee65e7d82a9c47
UrPrice <denis.korneevbk@gmail.com>
2024-10-18 01:14:24 +0300

    SS220 Atmos Threshold configued

--- Content.Server/Atmos/Piping/Unary/Components/GasVentPumpComponent.cs
+++ Content.Server/Atmos/Piping/Unary/Components/GasVentPumpComponent.cs
@@ -35,7 +35,7 @@ namespace Content.Server.Atmos.Piping.Unary.Components
         ///     In releasing mode, do not pump when environment pressure is below this limit.
         /// </summary>
         [DataField]
-        public float UnderPressureLockoutThreshold = 80; // this must be tuned in conjunction with atmos.mmos_spacing_speed
+        public float UnderPressureLockoutThreshold = 40; // SS220 Atmos Threshold configued // this must be tuned in conjunction with atmos.mmos_spacing_speed
 
         /// <summary>
         ///     Pressure locked vents still leak a little (leading to eventual pressurization of sealed sections)
@@ -58,7 +58,7 @@ namespace Content.Server.Atmos.Piping.Unary.Components
         [DataField]
         public bool IsPressureLockoutManuallyDisabled = false;
         /// <summary>
-        /// The time when the manual pressure lockout will be reenabled. 
+        /// The time when the manual pressure lockout will be reenabled.
         /// </summary>
         [DataField]
         [AutoPausedField]
