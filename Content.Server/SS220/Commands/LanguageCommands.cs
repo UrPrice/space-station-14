a3bfec2bf7a98ead5c9a467895c4648aee443129
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-03-07 20:25:14 +0300

    commands fix

--- Content.Server/SS220/Commands/LanguageCommands.cs
+++ Content.Server/SS220/Commands/LanguageCommands.cs
@@ -9,135 +9,170 @@ using Robust.Shared.Console;
 namespace Content.Server.SS220.Commands;
 
 [AdminCommand(AdminFlags.Admin)]
-public sealed class AddLanguageCommand : IConsoleCommand
+public sealed class AddLanguageCommand : LocalizedCommands
 {
-    [Dependency] private readonly IEntityManager _entities = default!;
+    [Dependency] private readonly IEntityManager _entManager = default!;
     [Dependency] private readonly LanguageManager _languageManager = default!;
 
-    public string Command => "addlanguage";
-    public string Description => Loc.GetString("cmd-language-add-desc");
-    public string Help => "addlanguage <entityId> <languageId> <canSpeak>";
+    public override string Command => "addlanguage";
 
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (args.Length != 3)
         {
-            shell.WriteError("addlanguage <entityId> <languageId> <canSpeak>");
+            shell.WriteError(Loc.GetString("shell-wrong-arguments-number"));
             return;
         }
 
-        if (!EntityUid.TryParse(args[0], out var entityId))
+        if (!NetEntity.TryParse(args[0], out var netEntity))
         {
-            shell.WriteError(Loc.GetString("cmd-language-invalid-id"));
+            shell.WriteError(Loc.GetString("shell-entity-uid-must-be-number"));
+            return;
+        }
+
+        if (!_entManager.TryGetEntity(netEntity, out var uid))
+        {
+            shell.WriteError(Loc.GetString("cmd-language-invalid-entity", ("uid", args[0])));
+            return;
+        }
+
+        var languageId = args[1];
+        if (!_languageManager.TryGetLanguageById(languageId, out _))
+        {
+            shell.WriteError(Loc.GetString("cmd-language-proto-miss", ("id", languageId)));
             return;
         }
 
         if (!bool.TryParse(args[2], out var canSpeak))
         {
+            shell.WriteError(Loc.GetString("cmd-addlanguage-can-speak-not-bool"));
             return;
         }
 
-        var languageId = args[1];
-
-        if (!_languageManager.TryGetLanguageById(languageId, out _))
+        var languageComp = _entManager.EnsureComponent<LanguageComponent>(uid.Value);
+        var languageSystem = _entManager.System<LanguageSystem>();
+        if (languageSystem.AddLanguage((uid.Value, languageComp), languageId, canSpeak))
         {
-            shell.WriteError(Loc.GetString("cmd-language-proto-miss"));
-            return;
-        }
-
-        if (!_entities.TryGetComponent<LanguageComponent>(entityId, out var languageComp))
-        {
-            shell.WriteError(Loc.GetString("cmd-language-comp-miss"));
-            return;
-        }
-
-        var languageSystem = _entities.System<LanguageSystem>();
-        if (languageSystem.AddLanguage((entityId, languageComp), languageId, canSpeak))
-        {
-            shell.WriteLine(Loc.GetString("cmd-language-success-add"));
+            shell.WriteLine(Loc.GetString("cmd-addlanguage-success-add"));
         }
         else
         {
-            shell.WriteLine(Loc.GetString("cmd-language-already-have"));
+            shell.WriteLine(Loc.GetString("cmd-addlanguage-already-have"));
         }
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        return args.Length switch
+        {
+            1 => CompletionResult.FromHint("cmd-language-entity-uid"),
+            2 => CompletionResult.FromHintOptions(CompletionHelper.PrototypeIDs<LanguagePrototype>(), Loc.GetString("cmd-language-id-list")),
+            3 => CompletionResult.FromHintOptions(CompletionHelper.Booleans, Loc.GetString("cmd-addlanguage-can-speak")),
+            _ => CompletionResult.Empty
+        };
     }
 }
 
 [AdminCommand(AdminFlags.Admin)]
-public sealed class RemoveLanguageCommand : IConsoleCommand
+public sealed class RemoveLanguageCommand : LocalizedCommands
 {
-    [Dependency] private readonly IEntityManager _entities = default!;
+    [Dependency] private readonly IEntityManager _entManager = default!;
 
-    public string Command => "removelanguage";
-    public string Description => Loc.GetString("cmd-language-remove-desc");
-    public string Help => "removelanguage <entityId> <languageId>";
+    public override string Command => "removelanguage";
 
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (args.Length != 2)
         {
-            shell.WriteError("removelanguage <entityId> <languageId>");
+            shell.WriteError(Loc.GetString("shell-wrong-arguments-number"));
             return;
         }
 
-        if (!EntityUid.TryParse(args[0], out var entityId))
+        if (!NetEntity.TryParse(args[0], out var netEntity))
         {
-            shell.WriteError(Loc.GetString("cmd-language-invalid-id"));
+            shell.WriteError(Loc.GetString("shell-entity-uid-must-be-number"));
+            return;
+        }
+
+        if (!_entManager.TryGetEntity(netEntity, out var uid))
+        {
+            shell.WriteError(Loc.GetString("cmd-language-invalid-entity", ("uid", args[0])));
             return;
         }
 
         var languageId = args[1];
-
-        if (!_entities.TryGetComponent<LanguageComponent>(entityId, out var languageComp))
+        if (!_entManager.TryGetComponent<LanguageComponent>(uid, out var languageComp))
         {
             shell.WriteError(Loc.GetString("cmd-language-comp-miss"));
             return;
         }
 
-        var languageSystem = _entities.System<LanguageSystem>();
-        if (languageSystem.RemoveLanguage((entityId, languageComp), languageId))
+        var languageSystem = _entManager.System<LanguageSystem>();
+        if (languageSystem.RemoveLanguage((uid.Value, languageComp), languageId))
         {
-            shell.WriteLine(Loc.GetString("cmd-language-succes-remove"));
+            shell.WriteLine(Loc.GetString("cmd-removelanguage-success"));
         }
         else
         {
-            shell.WriteLine(Loc.GetString("cmd-language-fail-remove"));
+            shell.WriteLine(Loc.GetString("cmd-removelanguage-fail"));
         }
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        return args.Length switch
+        {
+            1 => CompletionResult.FromHint("cmd-language-entity-uid"),
+            2 => CompletionResult.FromHintOptions(CompletionHelper.PrototypeIDs<LanguagePrototype>(), Loc.GetString("cmd-language-id-list")),
+            _ => CompletionResult.Empty
+        };
     }
 }
 
 [AdminCommand(AdminFlags.Admin)]
-public sealed class ClearLanguagesCommand : IConsoleCommand
+public sealed class ClearLanguagesCommand : LocalizedCommands
 {
-    [Dependency] private readonly IEntityManager _entities = default!;
+    [Dependency] private readonly IEntityManager _entManager = default!;
 
-    public string Command => "clearlanguages";
-    public string Description => Loc.GetString("cmd-language-clear-desc");
-    public string Help => "clearlanguages <entityId>";
+    public override string Command => "clearlanguages";
 
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
         if (args.Length != 1)
         {
-            shell.WriteError("clearlanguages <entityId>");
+            shell.WriteError(Loc.GetString("shell-wrong-arguments-number"));
             return;
         }
 
-        if (!EntityUid.TryParse(args[0], out var entityId))
+        if (!NetEntity.TryParse(args[0], out var netEntity))
         {
-            shell.WriteError(Loc.GetString("cmd-language-invalid-id"));
+            shell.WriteError(Loc.GetString("shell-entity-uid-must-be-number"));
             return;
         }
 
-        if (!_entities.TryGetComponent<LanguageComponent>(entityId, out var languageComp))
+        if (!_entManager.TryGetEntity(netEntity, out var uid))
+        {
+            shell.WriteError(Loc.GetString("cmd-language-invalid-entity", ("uid", args[0])));
+            return;
+        }
+
+        if (!_entManager.TryGetComponent<LanguageComponent>(uid, out var languageComp))
         {
             shell.WriteError(Loc.GetString("cmd-language-comp-miss"));
             return;
         }
 
-        var languageSystem = _entities.System<LanguageSystem>();
-        languageSystem.ClearLanguages((entityId, languageComp));
-        shell.WriteLine(Loc.GetString("cmd-language-clear"));
+        var languageSystem = _entManager.System<LanguageSystem>();
+        languageSystem.ClearLanguages((uid.Value, languageComp));
+        shell.WriteLine(Loc.GetString("cmd-clearlanguages-success"));
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        return args.Length switch
+        {
+            1 => CompletionResult.FromHint("cmd-language-entity-uid"),
+            _ => CompletionResult.Empty
+        };
     }
 }
-
--- Resources/Locale/ru-RU/ss220/language/language-cmd.ftl
+++ Resources/Locale/ru-RU/ss220/language/language-cmd.ftl
@@ -1,13 +1,25 @@
-# Desc
-cmd-language-add-desc = Добавляет язык в LanguageComponent энтити
-cmd-language-remove-desc = Удаляет язык из LanguageComponent энтити
-cmd-language-clear-desc = Удаляет все языки из LanguageComponent энтити
-# Message
-cmd-language-invalid-id = Некорректный энтити ID
+cmd-language-invalid-arguments = Неверное количество аргументов
+cmd-language-proto-miss = Не найден прототип языка с id: { $id }
+cmd-language-invalid-entity = Не найден энтити с id: { $uid }
+cmd-language-entity-uid = [id энтити]
+cmd-language-id-list = [id языка]
 cmd-language-comp-miss = У энтити отсутсвует LanguageComponent
-cmd-language-proto-miss = Некорректный прототип языка
-cmd-language-success-add = Язык успешно добавлен
-cmd-language-succes-remove = Язык удален
-cmd-language-fail-remove = Энтити не обладает таким языком
-cmd-language-already-have = Энтити уже знает этот язык
-cmd-language-clear = Все языки энтити - удалены
+
+# Add
+cmd-addlanguage-desc = Добавляет язык в LanguageComponent энтити
+cmd-addlanguage-help = Использование: addlanguage <entityUid> <languageId> <canSpeak>
+cmd-addlanguage-can-speak = [Может говорить]
+cmd-addlanguage-can-speak-not-bool = <canSpeak> должно быть true или false
+cmd-addlanguage-success-add = Язык успешно добавлен
+cmd-addlanguage-already-have = Энтити уже знает этот язык
+
+# Remove
+cmd-removelanguage-desc = Удаляет язык из LanguageComponent энтити
+cmd-removelanguage-help = Использование: removelanguage <entityId> <languageId>
+cmd-removelanguage-success = Язык удален
+cmd-removelanguage-fail = Энтити не обладает таким языком
+
+# Clear
+cmd-clearlanguages-desc = Удаляет все языки из LanguageComponent энтити
+cmd-clearlanguages-help = Использование: clearlanguages <entityId>
+cmd-clearlanguages-success = Все языки энтити - удалены
