7bd6cd459959afe1f0930d8317c47e724acf699a
krevetkqqq <161033382+krevetkqqq@users.noreply.github.com>
2025-08-15 20:38:15 +0300

    Реворк спавна пауков слуг (#3223)
    * .
    
    * .
    
    * .
    
    * Update Spider_actions.yml
    
    * Update spider_queen.yml
    
    * .
    
    * .
    
    * .

--- Content.Server/SS220/SpiderQueen/Systems/SpiderEggSystem.cs
+++ Content.Server/SS220/SpiderQueen/Systems/SpiderEggSystem.cs
@@ -1,7 +1,12 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+using Content.Server.Mind;
 using Content.Server.NPC;
 using Content.Server.NPC.Systems;
 using Content.Server.SS220.SpiderQueen.Components;
+using Content.Shared.Examine;
+using Content.Shared.Mind.Components;
+using Content.Shared.Speech;
+using Content.Shared.Spider;
 using Content.Shared.Storage;
 using Robust.Shared.Map;
 using Robust.Shared.Random;
@@ -13,7 +18,17 @@ public sealed partial class SpiderEggSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly NPCSystem _npc = default!;
-
+    [Dependency] private readonly MindSystem _mind = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<SpiderEggComponent, SpeakAttemptEvent>(OnTrySpeak);
+        SubscribeLocalEvent<SpiderEggComponent, ExaminedEvent>(OnExamine);
+    }
+    private void OnTrySpeak(EntityUid uid, SpiderEggComponent comp, ref SpeakAttemptEvent ev)
+    {
+        ev.Cancel();
+    }
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
@@ -38,8 +53,19 @@ public sealed partial class SpiderEggSystem : EntitySystem
         foreach (var proto in protos)
         {
             var ent = Spawn(proto, coordinates);
+            if (TryComp<MindContainerComponent>(uid, out var mind) && mind.HasMind)
+                _mind.TransferTo(mind.Mind.Value, ent); // transferto сам по себе если не может зарезолвить mind дает return, на проверки должно быть пофиг, но mind нужен
             if (component.EggOwner is { } owner)
                 _npc.SetBlackboard(ent, NPCBlackboard.FollowTarget, new EntityCoordinates(owner, Vector2.Zero));
+        }
+    }
+    private void OnExamine(EntityUid uid, SpiderEggComponent comp, ExaminedEvent ev)
+    {
+        var roundSec = (int)Math.Round(comp.IncubationTime);
+        var has = HasComp<SpiderComponent>(ev.Examiner); // у королевы все равно есть SpiderComponent
+        if (ev.Examiner == ev.Examined || has)
+        {
+            ev.PushMarkup(has ? Loc.GetString("SpiderEgg-IncubationYou", ("sec", roundSec)) : Loc.GetString("SpiderEgg-IncubationMe", ("sec", roundSec)));
         }
     }
 }
