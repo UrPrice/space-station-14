cc994d27973fa51807b9f1da61a156cac442c835
Anri <elyasin.ane@mail.ru>
2025-08-25 22:42:39 +0300

    Logs copy and filter addition (#3279)
    * init-commit
    
    * post-seld-review-fix
    
    * review-changes
    
    * hover-select-and-clear-selection-confirm
    
    * post-review-and-conf-button
    
    * skill-issue
    
    * Update Content.Client/Administration/UI/Logs/AdminLogsControl.SS220.xaml.cs
    
    ---------
    
    Co-authored-by: Kirus59 <145689588+Kirus59@users.noreply.github.com>

---
+++ Content.Client/SS220/UserInterface/Utility/ConfirmableButton.cs
@@ -0,0 +1,170 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Timing;
+using System.Threading.Tasks;
+
+namespace Content.Client.SS220.UserInterface.Utility;
+
+[Virtual]
+public class ConfirmableButton : Button
+{
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+
+    public Action? OnConfirmed;
+
+    [ViewVariables]
+    public ConfirmableButtonClicksAction ClicksActionWhenConfirmed = ConfirmableButtonClicksAction.Reset;
+    [ViewVariables]
+    public ConfirmableButtonClicksAction ClicksActionWhenNotConfirmed = ConfirmableButtonClicksAction.Reset;
+
+    [ViewVariables]
+    public float ConfirmDelayMillisecond { get; set; }
+
+    public TimeSpan ConfirmDelay => TimeSpan.FromMilliseconds(ConfirmDelayMillisecond);
+
+    [ViewVariables]
+    public uint ClicksForConfirm { get; set; }
+
+    [ViewVariables]
+    public string? DefaultText;
+    [ViewVariables]
+    public Color? DefaultColor;
+
+    private TimeSpan _lastClick = TimeSpan.Zero;
+
+    private int _loopedUpdateRate = 10;
+
+    private int _curClicks = 0;
+    private Dictionary<uint, ConfirmableButtonState> _clickStates = new();
+
+    public ConfirmableButton()
+    {
+        IoCManager.InjectDependencies(this);
+
+        OnPressed += _ => ProcessClick();
+        SetClickState(0, new ConfirmableButtonState(DefaultText, DefaultColor));
+        LoopedUpdate();
+    }
+
+    public ConfirmableButton(ConfirmableButtonState defaultState) : this()
+    {
+        SetClickState(0, defaultState);
+    }
+
+    public ConfirmableButton(string? text, Color? overrideColor) : this(new ConfirmableButtonState(text, overrideColor)) { }
+
+
+    public void SetClickState(Dictionary<uint, ConfirmableButtonState> clickStates)
+    {
+        foreach (var (key, value) in clickStates)
+        {
+            SetClickState(key, value);
+        }
+    }
+
+    public void SetClickState(uint click, ConfirmableButtonState state)
+    {
+        _clickStates[click] = state;
+        UpdateState();
+    }
+
+    public void Update()
+    {
+        if (Disposed)
+            return;
+
+        if (_curClicks >= ClicksForConfirm)
+            Confirmed();
+
+        if (_curClicks != 0 && _gameTiming.CurTime >= _lastClick + ConfirmDelay)
+            NotConfirmed();
+
+        UpdateState();
+    }
+
+    private async void LoopedUpdate()
+    {
+        await Task.Delay(1000 / _loopedUpdateRate);
+        LoopedUpdate();
+
+        if (_curClicks > 0)
+            Update();
+    }
+
+    private void UpdateState()
+    {
+        if (Disposed)
+            return;
+
+        if (_clickStates.TryGetValue((uint)_curClicks, out var state))
+        {
+            Text = state.Text;
+            ModulateSelfOverride = state.OverrideColor;
+        }
+    }
+
+    private void ProcessClick()
+    {
+        _lastClick = _gameTiming.CurTime;
+        IncreaseClicks();
+    }
+
+    private void Confirmed()
+    {
+        OnConfirmed?.Invoke();
+        ProcessActionWithClicks(ClicksActionWhenConfirmed);
+    }
+
+    private void NotConfirmed()
+    {
+        ProcessActionWithClicks(ClicksActionWhenNotConfirmed);
+    }
+
+    private void ResetClicks()
+    {
+        _curClicks = 0;
+        Update();
+    }
+
+    private void IncreaseClicks()
+    {
+        _curClicks++;
+        Update();
+    }
+
+    private void DecreaseClicks()
+    {
+        var newValue = _curClicks - 1;
+        _curClicks = Math.Max(newValue, 0);
+        Update();
+    }
+
+    private void ProcessActionWithClicks(ConfirmableButtonClicksAction action)
+    {
+        switch (action)
+        {
+            case ConfirmableButtonClicksAction.Decrease:
+                DecreaseClicks();
+                break;
+
+            case ConfirmableButtonClicksAction.Increase:
+                DecreaseClicks();
+                break;
+
+            case ConfirmableButtonClicksAction.Reset:
+                ResetClicks();
+                break;
+        }
+    }
+}
+
+public record struct ConfirmableButtonState(string? Text, Color? OverrideColor);
+
+public enum ConfirmableButtonClicksAction
+{
+    None,
+    Decrease,
+    Increase,
+    Reset
+}
