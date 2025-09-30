61fddfeb07915f404285543fa861ab167e0e07ce
ReeZer2 <63300653+ReeZer2@users.noreply.github.com>
2025-09-30 14:28:57 +0300

    TWEAK: Item offer prediction (#3404)

--- Content.Shared/SS220/ItemOffer/Verb/Event.cs
+++ Content.Shared/SS220/ItemOffer/Verb/Event.cs
@@ -1,7 +1,7 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using Content.Shared.Alert;
 
-namespace Content.Shared.SS220.ItemOfferVerb
-{
-    public sealed partial class ItemOfferAlertEvent : BaseAlertEvent;
-}
+namespace Content.Shared.SS220.ItemOffer.Verb;
+
+public sealed partial class ItemOfferAlertEvent : BaseAlertEvent;
