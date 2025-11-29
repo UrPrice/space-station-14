bf852d2a4bb641ab200210a053d0b9f56446861c
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-11-29 14:26:07 +0300

    Fix wiki entity effect description (#3656)
    * init
    
    * some rename

--- Content.Server/SS220/Wiki/Chemistry/EntityEffectWikiEntry.cs
+++ Content.Server/SS220/Wiki/Chemistry/EntityEffectWikiEntry.cs
@@ -1,6 +1,8 @@
 // Based on https://github.com/space-syndicate/space-station-14/blob/d69a4aa3d99a04cab64c8f807fea5e983e897866/Content.Server/Corvax/GuideGenerator/ReagentEffectEntry.cs
 using Content.Shared.EntityEffects;
+using Content.Shared.Localizations;
 using Robust.Shared.Prototypes;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace Content.Server.SS220.Wiki.Chemistry;
@@ -19,21 +21,38 @@ public sealed class EntityEffectWikiEntry
         var entSysMng = IoCManager.Resolve<IEntitySystemManager>();
 
         Id = effect.GetType().Name;
-        Description = GuidebookEffectDescriptionToWiki(effect.EntityEffectGuidebookText(protoMng, entSysMng) ?? "");
+        Description = GetEntityEffectWikiDescription(effect, protoMng, entSysMng);
     }
 
-    private string GuidebookEffectDescriptionToWiki(string guideBookText)
+    public static string GetEntityEffectWikiDescription(EntityEffect effect, IPrototypeManager protoMng, IEntitySystemManager entSysMng)
+    {
+        if (effect.EntityEffectGuidebookText(protoMng, entSysMng) is not { } effectDesc)
+            return string.Empty;
+
+        var fullDesc = Loc.GetString("wiki-entity-effect-description",
+            ("effect", effectDesc),
+            ("chance", effect.Probability),
+            ("conditionCount", effect.Conditions?.Length ?? 0),
+            ("conditions",
+                ContentLocalizationManager.FormatList(
+                    effect.Conditions?.Select(x => x.EntityConditionGuidebookText(protoMng)).ToList() ?? []
+                )));
+
+        return GuidebookEffectDescriptionToWiki(fullDesc);
+    }
+
+    private static string GuidebookEffectDescriptionToWiki(string guideBookText)
     {
         guideBookText = guideBookText.Replace("[", "<");
         guideBookText = guideBookText.Replace("]", ">");
         guideBookText = guideBookText.Replace("color", "span");
 
-        while (guideBookText.IndexOf("<span=") != -1)
+        while (guideBookText.Contains("<span=", StringComparison.CurrentCulture))
         {
             var first = guideBookText.IndexOf("<span=") + "<span=".Length - 1;
-            var last = guideBookText.IndexOf(">", first);
-            var replacementString = guideBookText.Substring(first, last - first);
-            var color = replacementString.Substring(1);
+            var last = guideBookText.IndexOf('>', first);
+            var replacementString = guideBookText[first..last];
+            var color = replacementString[1..];
             guideBookText = guideBookText.Replace(replacementString, string.Format(" style=\"color: {0};\"", color));
         }
 
