fb407b13d2496eccabb038a44e69da87f39b3eeb
SIXSEVENONE <65928200+SIXSEVENONE@users.noreply.github.com>
2026-01-25 13:56:40 +0100

    Remaining accents moved to component accent system (#3851)
    * Added words for french accent
    
    * All remaining accents moved to accent system with components
    
    * Removed comments from SS220 maps
    
    * Removed french accent from this PR

---
+++ Content.Server/SS220/Speech/EntitySystems/LiarAccentSystem.cs
@@ -0,0 +1,28 @@
+// EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
+using Content.Server.Speech.EntitySystems;
+using Content.Server.SS220.Speech.Components;
+using Content.Shared.Speech;
+
+namespace Content.Server.SS220.Speech.EntitySystems;
+
+public sealed class LiarAccentSystem : EntitySystem
+{
+    [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<LiarAccentComponent, AccentGetEvent>(OnAccent);
+    }
+
+    private string Accentuate(string message)
+    {
+        return _replacement.ApplyReplacements(message, "liar");
+    }
+
+    private void OnAccent(Entity<LiarAccentComponent> ent, ref AccentGetEvent args)
+    {
+        args.Message = Accentuate(args.Message);
+    }
+}
