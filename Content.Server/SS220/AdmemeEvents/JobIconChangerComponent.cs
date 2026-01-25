a2ac9b1b67c9743710a6b382ea875309800064ad
UrPrice <denis.korneevbk@gmail.com>
2026-01-25 07:29:52 +0300

    Revert "Test Changes"
    This reverts commit 0a2ccd7d53bb31a0a327cb77fe39baa503f8e289.

--- Content.Server/SS220/AdmemeEvents/JobIconChangerComponent.cs
+++ Content.Server/SS220/AdmemeEvents/JobIconChangerComponent.cs
@@ -1,4 +1,4 @@
-// © SS220, An EULA/CLA with a hosting restriction , full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
 using Content.Shared.SS220.AdmemeEvents;
 using Content.Shared.StatusIcon;
